3187e9094daec1f8cd5a8c9c4fde3f028fe5f8bc
veryspry <ehlinger.matt@gmail.com>
2020-11-19 15:34:42 -0600

    feat: style the error modal contents

--- src/OpenVSCodeButton.tsx
+++ src/OpenVSCodeButton.tsx
@@ -2,20 +2,52 @@ import { exec } from 'child_process';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { ipcRenderer } from 'electron';
-// import type { Site } from '@getflywheel/local/main'
-
-// https://getflywheel.github.io/local-addon-api/modules/_local_renderer_.html
-import * as LocalRenderer from '@getflywheel/local/renderer';
-
-// https://github.com/getflywheel/local-components
 import { TextButton, FlyModal, Title, Text } from '@getflywheel/local-components';
 
 interface IOpenVSCodeButtonProps {
 	site: any;
 }
 
-const vsCodeUrl = 'https://code.visualstudio.com/';
-const codeCliUrl = 'https://code.visualstudio.com/docs/editor/command-line';
+const renderErrorModal = () => {
+	const vsCodeUrl = 'https://code.visualstudio.com/';
+	const codeCliUrl = 'https://code.visualstudio.com/docs/editor/command-line';
+
+	ReactDOM.render(
+		(
+			<FlyModal
+				contentLabel='Error opening VSCode'
+			>
+				<div className="VSCode_Addon_Modal">
+					<Title>Uh-oh! Looks like there was an error trying to open VSCode</Title>
+					<Text>
+						This add-on relies on
+						<TextButton
+							onClick={() => ipcRenderer.send('browserService:launch', null, vsCodeUrl)}
+							className="VSCode_Addon_Link_TextButton"
+							privateOptions={{
+								textTransform: 'none',
+							}}
+						>
+							{' VSCode '}
+						</TextButton>
+						and the
+						<TextButton
+							onClick={() => ipcRenderer.send('browserService:launch', null, codeCliUrl)}
+							className="VSCode_Addon_Link_TextButton"
+							privateOptions={{
+								textTransform: 'none',
+							}}
+						>
+							{' '}code{' '}
+						</TextButton>
+						CLI to run. Ensure you have both of those installed and try again.
+					</Text>
+				</div>
+			</FlyModal>
+		), document.getElementById('popup-container'),
+	);
+}
+
 
 export const OpenVSCodeButton: React.FC<IOpenVSCodeButtonProps> = (props) => {
 	const { site } = props;
@@ -29,39 +61,11 @@ export const OpenVSCodeButton: React.FC<IOpenVSCodeButtonProps> = (props) => {
 
 		exec(cmd, (err, stdout, stderr) => {
 			setIsOpening(false);
-			console.log(err, stdout, stderr);
 
 			if (err) {
 				renderErrorModal();
 			}
 		});
-	}
-
-	const renderErrorModal = () => {
-		ReactDOM.render(
-			(
-				<FlyModal
-					contentLabel='Confirm Optimization'
-				>
-					<Title>Uh-oh! Looks like there was an error trying to open VSCode</Title>
-					<Text>
-						This add-on relies on
-						<TextButton
-							onClick={() => ipcRenderer.send('browserService:launch', null, vsCodeUrl)}
-						>
-							{' '}VSCode{' '}
-						</TextButton>
-						and the
-						<TextButton
-							onClick={() => ipcRenderer.send('browserService:launch', null, codeCliUrl)}
-						>
-							{' '}code{' '}
-						</TextButton>
-						CLI to run.
-					</Text>
-				</FlyModal>
-			), document.getElementById('popup-container'),
-		);
 	}
 
 	return (
